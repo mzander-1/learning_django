590d8451656142f743a91951219044a865beedf9
mzander <mike.1707@hotmail.de>
2023-09-08 13:57:02 +0200

    init django, added urls, added fonds, added html

---
+++ shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def shop(request):
+    return render(request, "shop/shop.html")
+
+def warenkorb(request):
+    return render(request, "shop/warenkorb.html")
+
+def kasse(request):
+    return render(request, "shop/kasse.html")
